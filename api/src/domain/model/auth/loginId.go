59cfd9d9c75815007c12168843c7c51be8738393
mountain-mouth <yamataku0807@gmail.com>
2024-06-21 00:00:45 +0900

    dev/loginIdオブジェクト作りかけ

--- api/src/domain/model/auth/loginId.go
+++ api/src/domain/model/auth/loginId.go
@@ -1,1 +1,23 @@
-package domain/
+package auth
+
+import (
+	"errors"
+	"regexp"
+)
+
+type LoginId struct {
+	value string
+}
+
+func NewLoginId(value string)(*LoginId, error){
+	if !validate(value){
+		return nil, errors.New("Invalid id format")
+	}
+	return &LoginId{value: value},nil
+
+}
+
+func validate(value string) bool {
+	regex := regexp.MustCompile(`^[a-zA-Z0-9]{3,20}$`)
+	return regex.MatchString(value)
+}
