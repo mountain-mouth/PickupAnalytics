4ccd087f70d95ad41d5bb7a4e4d189e407b4d9cc
榑林晃平 <kouhei.kurebayashi@goals.co.jp>
2024-06-21 02:02:05 +0900

    goとポスぐれの通信

--- api/src/main.go
+++ api/src/main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-
 	"github.com/gin-gonic/gin"
-
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
 )
 
-// Product type
-type Product struct {
+type Creature struct {
 	ID    uint   `json:”id”`
 	Code  string `json:”code”`
-	Price uint   `json:”price”`
+	Name string   `json:”name”`
 }
 
 func main() {
 
 	r := gin.Default()
+
+	// テストデータ作成
+	InsertTestData()
 
 	r.GET("/hello", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -49,28 +50,52 @@ func main() {
 		})
 	})
 
-	r.GET("/products", GetProducts)
+	r.GET("/creature", GetCreature)
 
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
 
-func GetProducts(c *gin.Context) {
+func InitDB() *gorm.DB {
+	dsn := "host=172.28.1.3 user=postgres password=postgres dbname=pickup_analytics port=5432 sslmode=disable TimeZone=Asia/Tokyo"
+    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatalf("failed to connect database: %v", err)
+    }
+	return db
+}
 
-	db, err := gorm.Open("sqlite3", "test.db")
-	if err != nil {
-		panic("failed to connect database")
+func InsertTestData() {
+	db := InitDB()
+	db.AutoMigrate(&Creature{})
+
+	testCreaturetData := []Creature{
+		{Code: "L1212", Name: "うみねこ"},
+		{Code: "L1213", Name: "うり"},
+		{Code: "L1214", Name: "ねこ"},
 	}
-	defer db.Close()
-	db.AutoMigrate(&Product{})
 
-	var products []Product
+	for _, creature := range testCreaturetData {
+		result := db.Create(&creature)
 
-	if err := db.Find(&products).Error; err != nil {
+		if result.Error != nil {
+			fmt.Println("エラー:", result.Error)
+		} else {
+			fmt.Println("新しい商品のID:", creature.Name)
+		}
+	} 
+}
+
+func GetCreature(c *gin.Context) {
+	db := InitDB()
+
+	var creature []Creature
+
+	if err := db.Find(&creature).Error; err != nil {
 		c.AbortWithStatus(http.StatusInternalServerError)
 		log.Println(err)
 	} else {
-		c.JSON(http.StatusOK, products)
-		log.Println(products)
+		c.JSON(http.StatusOK, creature)
+		log.Println(creature)
 	}
 
 }
