b945bd5f5b0af3c62573778728ffd1b566834920
榑林晃平 <kouhei.kurebayashi@goals.co.jp>
2024-06-24 03:52:42 +0900

    要らない処理削除

--- api/main.go
+++ api/main.go
@@ -29,28 +29,6 @@ func main() {
 		})
 	})
 
-	r.GET("/echo/:echo", func(c *gin.Context) {
-		echo := c.Param("echo")
-		c.JSON(http.StatusOK, gin.H{
-			"echo": echo,
-		})
-	})
-
-	r.POST("/upload", func(c *gin.Context) {
-		form, _ := c.MultipartForm()
-		files := form.File["upload[]"]
-
-		for _, file := range files {
-			log.Println(file.Filename)
-
-			// Upload the file to specific dst.
-			// c.SaveUploadedFile(file, dst)
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"uploaded": len(files),
-		})
-	})
-
 	r.GET("/areas", func(c *gin.Context) {
         handler := &HandlerWithDB{DB: InitDB()}
         handler.GetAreaWrapper(c)
