063640cecffb3cce862efaef9d3691e0298d5e17
榑林晃平 <kouhei.kurebayashi@goals.co.jp>
2024-06-25 14:22:39 +0900

    サンプルコード作成

---
+++ front/src/app/home/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the home page!</p>
+    </div>
+  );
+}
---
+++ front/src/app/register/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1>ユーザー登録ページ</h1>
+    </div>
+  );
+}
